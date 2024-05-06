f7e7b977ad1af72ca8ae3a99429eb7af3b7d6ea5
Giusstation3 <giuseppevalencia28@gmail.com>
2024-05-06 15:23:40 -0600

    FirstLayout

---
+++ FastingApp.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import ttk
+
+# create a window 
+window = tk.Tk()
+window.title('Fasting')
+window.geometry('200x200')
+
+# widgets 
+label = ttk.Label(master = window, text = '')
+label.pack()
+
+
+entry = ttk.Entry(master = window,width=5,font=('Arial',40),justify='center',)
+entry.pack()
+entry.bind('<FocusOut>', lambda event: calculate_hours(entry.get()))
+
+
+entry2 = ttk.Entry(master = window,width=5,font=('Arial',40),justify='center',)
+entry2.pack()
+
+def calculate_hours(text):
+    res = "15hrs"
+    entry2.insert(0, res)
+
+def button_func():
+	# get the content of the entry
+	entry_text = entry.get()
+
+	# update the label
+	# label.configure(text = 'some other text')
+	label['text'] = entry_text
+	entry['state'] = 'disabled'
+	# print(label.configure())
+ 
+
+
+button = ttk.Button(master = window, text = 'Comenzar Ayuno', command = button_func)
+button.pack()
+
+# run 
+window.mainloop()
